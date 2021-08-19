467ba56e2c6036324ac375b7cc5c175c261e4c7b
MatteoGioioso <gioioso.matteo@gmail.com>
2021-08-19 17:23:18 +0700

    add eks roles with aws sso

---
+++ eks/0-create-cluster.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -e
+
+CLUSTER_NAME=eks-boilerplate
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo "Creating AWS EKS cluster...this might take a while"
+eksctl create cluster -f "$SCRIPT_PATH"/cluster.yaml --kubeconfig "$HOME"/.kube/"${CLUSTER_NAME}"
+
+echo "Cluster created, your kube config located at $KUBECONFIG"
+echo "Waiting for resources"
+sleep 10
+
+echo "Deploying Nginx ingress controller and Load balancer"
+helm install ingress-nginx ingress-nginx/ingress-nginx -f "$SCRIPT_PATH"/nginx-values.yaml
